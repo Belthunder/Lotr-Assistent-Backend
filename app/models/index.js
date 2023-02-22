8710b9810f7fbf3ccd713064a749404c18ca3911
Belthunder <chmball3@gmail.com>
2023-02-21 20:42:26 -0800

    Update index.js
    another syntax switch error.

--- app/models/index.js
+++ app/models/index.js
@@ -26,13 +26,13 @@ db.decks = require("./deck.model.js")(sequelize, Sequelize);
 
 db.cards.belongsToMany(db.decks, {
     through: "cards_in_decks",
-    as: "cards",
+    as: "decks",
     foreignKey: "card_id",
 });
 
 db.decks.belongsToMany(db.cards, {
     through: "cards_in_decks",
-    as: "decks",
+    as: "cards",
     foreignKey: "deck_id",
 });
 
